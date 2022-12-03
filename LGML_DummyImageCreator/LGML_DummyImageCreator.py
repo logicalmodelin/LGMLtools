b39b4c94f1b4e4224b5a991780e0b5263416144d
logicalmodelin <logicalmodelin@gmail.com>
2022-12-03 14:45:38 +0900

    update LGML_DummyImageCreator

--- LGML_DummyImageCreator/LGML_DummyImageCreator.py
+++ LGML_DummyImageCreator/LGML_DummyImageCreator.py
@@ -5,10 +5,29 @@ import argparse
 from typing import List, Tuple, Any, ClassVar, Literal, Callable, Union
 
 
+def _replace_text(text: str, size: tuple[int, int], index: str) -> str:
+    return text.replace(
+        "{w}", str(size[0])).replace(
+        "{h}", str(size[1])).replace(
+        "{i}", index).replace(
+        "\\n", "\n")
+
+
+def _get_text_size(text: str, font: ImageFont, spacing : int = 2) -> Tuple[int, int]:
+    contents_width: int = 0
+    s: str
+    for s in text.split("\n"):
+        contents_width = max(contents_width, font.getbbox(s)[2])
+    contents_height: int = font.getbbox(text)[3]
+    contents_height *= (text.count("\n") + 1)
+    contents_height += spacing * text.count("\n")
+    return contents_width, contents_height
+
+
 def main() -> None:
     parser: argparse.ArgumentParser = argparse.ArgumentParser(
-        usage='ダミー画像を生成する。',
-        description='適当な数とサイズでダミー画像を作ります。',
+        description='指定された枚数とサイズでダミー画像を生成します。 '
+                    'テキスト系の指定は英語のみで、{i}は連番番号に{w}は横幅に{h}は縦幅に変換されます。\nで改行指定も可能です。',
     )
     parser.add_argument("output_path", type=str, help="画像をアウトプットするフォルダ")
     parser.add_argument("width", type=int, help="出力画像の横幅")
@@ -21,11 +40,14 @@ def main() -> None:
     parser.add_argument("-tc", "--text_color", type=str, default="",
                         help="文字色の指定(16進数6文字) 指定ない場合背景色の補色")
     parser.add_argument("-t", "--title", type=str, default="[DUMMY IMAGE]\n{w}x{h}",
-                        help="画像に埋め込むタイトル文字 英語のみ {w}は横幅に{h}は縦幅に変換される")
-    parser.add_argument("--text_contents", type=str, default="", help="画像右下に埋め込む本文文字 英語のみ")
+                        help="画像に埋め込むタイトル文字")
+    parser.add_argument("-zp", "--zero_padding", type=int, default=3, help="画像INDEX番号を0でうめる桁数。")
+    parser.add_argument("--center_text", type=str, default="{i}", help="画像中央に埋め込む文字")
+    parser.add_argument("--bottom_contents", type=str, default="", help="画像右下に埋め込む文字")
     parser.add_argument("--force", action="store_true", help="上書き確認せずファイルを書き出すか")
     # デフォルトビットマップフォントがかっこいいが、サイズ指定できない
     # parser.add_argument("-fs", "--font_size", type=int, default=24, help="タイトル文字の大きさ")
+    parser.add_argument("-V", '--version', action='version', version='%(prog)s 1.1')
 
     args = parser.parse_args()
     output_path: Path = Path(args.output_path)
@@ -52,7 +74,6 @@ def main() -> None:
     else:
         text_color = \
             (int(text_color_str[0:2], base=16), int(text_color_str[2:4], base=16), int(text_color_str[4:6], base=16))
-    line_color: tuple[int, int, int] = text_color
     font: ImageFont = ImageFont.load_default()
     # font = ImageFont.truetype("arial.ttf", args.font_size)
 
@@ -66,24 +87,33 @@ def main() -> None:
         xx: int = small_size[0] - 2 - 1
         yy: int = small_size[1] - 2 - 1
         draw.rectangle((2, 2, xx, yy), outline=text_color, width=1)
-        # draw.line(((2, 2), (xx, yy)), fill=line_color)
-        draw.line(((2, yy), (xx, 2)), fill=line_color)
-        title: str = args.title.replace("{w}", str(size[0])).replace("{h}", str(size[1])).replace("\\n", "\n")
+        # line_color: tuple[int, int, int] = text_color
+        # draw.line(((2, 2), (xx, yy)), fill=line_color)  # ななめ線
+        # draw.line(((2, yy), (xx, 2)), fill=line_color)  # ななめ線
+        index_with_zero_pad = str(index).zfill(args.zero_padding)
+        # 左上のタイトル文字
+        title: str = _replace_text(args.title, size, index_with_zero_pad)
         draw.text(
             (6, 6), "{}".format(title, size[0], size[1]),
-            text_color, spacing=3, align='left', font=font
+            text_color, spacing=2, align='left', font=font
         )
-        text_contents: str = args.text_contents
-        # print(text_contents)
-        text_contents_size = font.getbbox(text_contents)
-        # print(text_contents_size)
+        # 右下の文字
+        bottom_contents: str = _replace_text(args.bottom_contents, size, index_with_zero_pad)
+        bottom_contents_size = _get_text_size(bottom_contents, font, spacing=2)
         draw.text(
-            (small_size[0] - text_contents_size[2] - 6, small_size[1] - text_contents_size[3] - 6),
-            text_contents, text_color
+            (small_size[0] - bottom_contents_size[0] - 6, small_size[1] - bottom_contents_size[1] - 6),
+            bottom_contents, text_color, spacing=2, align='right'
+        )
+        # 中央の文字
+        center_text: str = _replace_text(args.center_text, size, index_with_zero_pad)
+        center_text_size = _get_text_size(center_text, font, spacing=2)
+        draw.text(
+            (small_size[0] / 2 - center_text_size[0] / 2, small_size[1] / 2 - center_text_size[1] / 2),
+            center_text, text_color, spacing=2, align='center', font=font
         )
         # 小さく作って拡大している 文字の見栄えの調整
         image = image.resize(size, resample=Image.Resampling.NEAREST)
-        index_str: str = "#{}".format(index) if args.number_of_image > 1 else ""
+        index_str: str = "#{}".format(index_with_zero_pad) if args.number_of_image > 1 else ""
         output: Path = output_path / Path(filename_template.format(size[0], size[1], index_str))
         if output.exists() and not args.force:
             res: str = input("{}はすでに存在します。処理を進める場合 Y と入力して下さい。".format(output.name))
--- LGML_DummyImageCreator/README.md
+++ LGML_DummyImageCreator/README.md
@@ -1,10 +1,9 @@
 # LGML_DummyImageCreator
 
-ダミー画像を大量に生成してくれるやつです。
-
+ダミー画像を大量に生成してくれるやつです。 
 Pillowが必要です。Python3のいくつかで動きます。
 
-基本的な機能
+## 基本的な機能
 
 * 画像サイズ指定可能
 * 画像の枚数指定可能
@@ -12,24 +11,51 @@ Pillowが必要です。Python3のいくつかで動きます。
 * 生成枚数の指定
 * 画像フォーマット指定（PILの扱えるもののみ）
 
-その他の機能
+## その他の機能
 
 * タイトル文字指定可能
 * 背景色指定可能
 * 指定ない場合、文字色は背景色の補色に
+* 中央に画像連番文字配置
 * 右下に追加文字指定可能
+
+## 注意点
+
+* 文字列に改行埋め込み時は文字列引数を ”” でくくって下さい。
 
 外部フォント読み込みたくないので以下の制限があります。
 
 * テキストの大きさとフォント指定不可
 * 日本語使用不可
 
-コマンドヘルプそのまま
+# 更新履歴
+
+* 2022/12/3 v1.1
+  * バージョン表記を追加
+  * 画面中央文字列の指定が可能に
+  * 画像の中央にデフォルトで連番を表示・ななめ線描画の廃止
+  * 連番数値の0のパディング数指定が可能、ファイル名にも有効
+  * 各文字列で画像サイズの変数埋め込みと改行に対応
+  * その他デザイン調整
+
+* 2022/12/2 v1.0
+  * 初回リリース
+
+
+# コマンドヘルプ
 
 ```
-usage: ダミー画像を生成する。
+usage: LGML_DummyImageCreator.py [-h] [-n NUMBER_OF_IMAGE] [-p PREFIX]
+                                 [-s SUFFIX] [-fmt FORMAT] [-c COLOR]
+                                 [-tc TEXT_COLOR] [-t TITLE]
+                                 [-zp ZERO_PADDING]
+                                 [--center_text CENTER_TEXT]
+                                 [--bottom_contents BOTTOM_CONTENTS] [--force]
+                                 [-V]
+                                 output_path width height
 
-適当な数とサイズでダミー画像を作ります。
+指定された枚数とサイズでダミー画像を生成します。 テキスト系の指定は英語のみで、{i}は連番番号に{w}は横幅に{h}は縦幅に変換されます。
+で改行指定も可能です。
 
 positional arguments:
   output_path           画像をアウトプットするフォルダ
@@ -48,21 +74,26 @@ optional arguments:
                         エクスポート画像フォーマットの指定
   -c COLOR, --color COLOR
                         色の指定(16進数6文字)
+  -tc TEXT_COLOR, --text_color TEXT_COLOR
+                        文字色の指定(16進数6文字) 指定ない場合背景色の補色
   -t TITLE, --title TITLE
-                        画像に埋め込むタイトル文字 英語のみ
-  --text_contents TEXT_CONTENTS
-                        画像右下に埋め込む本文文字 英語のみ
+                        画像に埋め込むタイトル文字
+  -zp ZERO_PADDING, --zero_padding ZERO_PADDING
+                        画像INDEX番号を0でうめる桁数。
+  --center_text CENTER_TEXT
+                        画像中央に埋め込む文字
+  --bottom_contents BOTTOM_CONTENTS
+                        画像右下に埋め込む文字
   --force               上書き確認せずファイルを書き出すか
+  -V, --version         show program's version number and exit
 ```
 
-コマンド例
+# コマンド例
 
-* `py LGML_DummyImageCreator.py work 720 360 -t "Hi there," --force -c 000066 -tc 555555 --text_contents "How do you do today?"`
-* `py LGML_DummyImageCreator.py work 128 128 -n 2 --force -c ffffCC -fmt jpg`
-* `py LGML_DummyImageCreator.py work 640 427 --force -c eeffff -t "[DUMMY IMAGE CREATOR]\n{w}px / {h}px" -p header_ --text_contents "Creates dummy images easily."`
-* `py LGML_DummyImageCreator.py work 1280 720 --force -c eeeeee -t "[HD SIZE]" -p hd_ --text_contents "A snake sneaks to seek a snack."`
-
-
-
+* `python LGML_DummyImageCreator.py work 128 128 -n 2 --force -c ffffCC -fmt jpg`
+* `python LGML_DummyImageCreator.py work 720 360 -t "Hey, dude." --force -c 000066 -tc 555555 --bottom_contents "What's up, dude?"`
+* `python LGML_DummyImageCreator.py work 640 427 --force -c eeffff -t "[DUMMY IMAGE CREATOR]\n{w}px * {h}px" -p "header_" --bottom_contents "Creates dummy images easily."`
+* `python LGML_DummyImageCreator.py work 1280 720 --force -c eeeeee -t "[HD SIZE]" -p hd_ --bottom_contents "A snake sneaks to seek a snack."`
+* `python LGML_DummyImageCreator.py work 640 427 --force -c fff8ee -t "How much wood would\na woodchuck chuck" --center_text "Images\n#{i}" -p "sample_" --bottom_contents "if a woodchuck could\nchuck wood?"`
 
 
