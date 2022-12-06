5c0a7b48ef88719a296114a67fedc4e8227c3484
logicalmodelin <logicalmodelin@gmail.com>
2022-12-07 02:00:37 +0900

    バッチファイルサンプル変換前にコピーを取る

--- LGML_ImageSizeAdjuster/320x320に変換パディングあり.bat
+++ LGML_ImageSizeAdjuster/320x320に変換パディングあり.bat
@@ -1,6 +1,8 @@
-for %%I IN ( %1 ) do set "DIRNAME=%%~dpI"
 call  ..\venv\Scripts\activate.bat
+REM copy %1 %~dp1%~n1.bkup%~x1  // 変換すると前の状態が残らずゴミ箱にも入らないので不安であればバックアップを取る
 python %~dp0LGMLImageSizeAdjuster.py %1 -s 320 320 --force --preferred_direction AUTO_PAD
+
+
 
 
 
