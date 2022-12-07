bfa8813d99a269cf7f29b8de38135f33aa6048fe
Logical Modelin <101558906+logicalmodelin@users.noreply.github.com>
2022-12-07 12:26:56 +0900

    Update 640x427BlogHeader画像を作成トリムあり.bat

--- LGML_ImageSizeAdjuster/640x427BlogHeader画像を作成トリムあり.bat
+++ LGML_ImageSizeAdjuster/640x427BlogHeader画像を作成トリムあり.bat
@@ -1,4 +1,3 @@
-for %%I IN ( %1 ) do set "DIRNAME=%%~dpI"
 call  ..\venv\Scripts\activate.bat
 python %~dp0LGMLImageSizeAdjuster.py %1 -s 640 427 --force --preferred_direction AUTO_CROP -o "{p}/{n}_640x427.png"
 
