b2ba7acf46e8442429005a9c7377ef7fda56626a
logicalmodelin <logicalmodelin@gmail.com>
2023-01-29 08:05:20 +0900

    パスの修正

--- LGML_ImageSizeAdjuster/320x320に変換パディングあり.bat
+++ LGML_ImageSizeAdjuster/320x320に変換パディングあり.bat
@@ -1,8 +1,3 @@
-call  ..\..\venv\Scripts\activate.bat
-REM copy %1 %~dp1%~n1.bkup%~x1  // 変換すると前の状態が残らずゴミ箱にも入らないので不安であればバックアップを取る
+call ..\venv\Scripts\activate.bat
 python %~dp0LGMLImageSizeAdjuster.py %1 -s 320 320 --force --preferred_direction AUTO_PAD
-
-
-
-
-
+pause
